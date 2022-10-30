6ebbd98b449ccb6a71ae9b931b7f9a5e484d1ef4
Helen <helen2fyne2010@gmail.com>
2022-10-30 14:02:34 +0100

    added new file

---
+++ pythonSelenium2/main.py
@@ -0,0 +1,7 @@
+#Visit the website https://academy.testifyltd.com/
+#Locate the button with the text "Explore Courses" and print out the element
+#Locate the element with the text "Subscribe to receive training updates from Testify" and print it.
+#You can use any Location strategies of your choice
+
+
+from selenium import webdriver
